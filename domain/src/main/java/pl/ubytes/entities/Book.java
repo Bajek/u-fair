c3abeac3df1bd25bd1c2a6eff1338a8e01cb50ec
Bajek <bartek.urbaniec@gmail.com>
2015-12-05 09:24:02 +0100

    Added Book :)

---
+++ domain/src/main/java/pl/ubytes/entities/Book.java
@@ -0,0 +1,12 @@
+package pl.ubytes.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by Bajek on 05.12.2015.
+ */
+@Entity
+@Table(name = "Books")
+public class Book {
+}
