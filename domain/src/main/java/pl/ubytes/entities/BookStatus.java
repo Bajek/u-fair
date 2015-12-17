1d00bef48d85da33e21e7d3b901d5143354e8eee
Bajek <bartek.urbaniec@gmail.com>
2015-12-17 23:35:45 +0100

    Enhanced book status

--- domain/src/main/java/pl/ubytes/entities/BookStatus.java
+++ domain/src/main/java/pl/ubytes/entities/BookStatus.java
@@ -2,9 +2,11 @@ package pl.ubytes.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.sql.Timestamp;
 
 /**
- * Created by Bartłomiej on 07.12.2015.
+ * Created: Bajek
+ * Date: 107.12.2015.
  */
 @Entity
 @Table(name = "Status")
@@ -21,9 +23,45 @@ public class BookStatus implements Serializable {
     Book book;
 
     private String buyer;
+    private String ip;
+    private String comments;
+    private Timestamp buyDate;
+    private String email;
 
 
     public BookStatus() {
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public Timestamp getBuyDate() {
+        return buyDate;
+    }
+
+    public void setBuyDate(Timestamp buyDate) {
+        this.buyDate = buyDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getBuyer() {
